97fc83f516963f639711397d82598e5a57b02129
Bob Kozdemba <bkozdemba@gmail.com>
2023-09-07 04:42:51 +0000

    Added main

--- 01-sentiment-analysis.py
+++ 01-sentiment-analysis.py
@@ -1,11 +1,14 @@
 from transformers import pipeline
 import gradio as gr
 
-classifier = pipeline("sentiment-analysis")
 
 def predict(prompt):
     return classifier([prompt])
 
-examples = ["The book was well written with an exciting plot."]
-demo = gr.Interface(fn=predict, inputs="text", outputs="text", examples=examples)
-demo.launch()
+if __name__ == "__main__":
+
+    classifier = pipeline("sentiment-analysis")
+
+    examples = ["The book was well written with an exciting plot."]
+    demo = gr.Interface(fn=predict, inputs="text", outputs="text", examples=examples)
+    demo.launch()

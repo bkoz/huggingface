1ca4b07b7f307cf50f2752149487fed9974c8afa
Bob Kozdemba <bkozdemba@gmail.com>
2023-09-07 03:11:24 +0000

    updated

---
+++ 01-sentiment-analysis.py
@@ -0,0 +1,9 @@
+from transformers import pipeline
+
+classifier = pipeline("sentiment-analysis")
+classifier(
+    [
+        "I've been waiting for a HuggingFace course my whole life.",
+        "I hate this so much!",
+    ]
+)
